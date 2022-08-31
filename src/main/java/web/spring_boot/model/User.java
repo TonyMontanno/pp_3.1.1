54d8309c5fca5e5d7f35fae8fa915a196c94c1bc
Anton Yun <Crazy_antonio@mail.ru>
2022-08-31 19:20:41 +0300

    Initial commit

---
+++ src/main/java/web/spring_boot/model/User.java
@@ -0,0 +1,22 @@
+package web.spring_boot.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+}
